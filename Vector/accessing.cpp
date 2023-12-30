8161473be1d0bc81507f4c030a087f6c0f6b040b
masum184e <masum184e@gmail.com>
2023-12-30 15:00:26 +0600

    vector item accessing added

---
+++ Vector/accessing.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> initial_value_vector = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    // INDEX
+    initial_value_vector[5];
+    // we can't add value through index
+    // we can modify the indexed value if it initialized
+
+    // FRONT & BACK
+    initial_value_vector.front();
+    initial_value_vector.back();
+
+    // LOOP
+    for(auto i:initial_value_vector)cout<<i<<" ";
+
+    return 0;
+}
