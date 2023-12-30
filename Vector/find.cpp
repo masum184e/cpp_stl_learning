f735309eeaeef7683184dd71cdb215d20863e135
masum184e <masum184e@gmail.com>
2023-12-30 14:36:54 +0600

    specific element finding added

--- Vector/find.cpp
+++ Vector/find.cpp
@@ -3,11 +3,16 @@ using namespace std;
 int main(){
     vector<int> sized_vector(10);
 
+    // FIND SPECIFIC ELEMENT
+    auto iterator=find(sized_vector.begin(), sized_vector.end(), 456);
+    if(iterator!=sized_vector.end())cout<<"Element found at index "<<iterator-sized_vector.begin()<<endl;
+    else cout<<"Element not found in the vector."<<endl;
+
     // FIND MAXIMUM ELEMENT
-    cout<<"Max Element = "<<*max_element(a.begin(),a.end())<<endl;
+    cout<<"Max Element = "<<*max_element(sized_vector.begin(),sized_vector.end())<<endl;
 
     // FIND MINIMUM ELEMENT
-    cout<<"Min Element = "<<*min_element(a.begin(),a.end())<<endl;
+    cout<<"Min Element = "<<*min_element(sized_vector.begin(),sized_vector.end())<<endl;
  
     return 0;
 }
