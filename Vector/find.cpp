13ef26d84a82e8bb565074873060fce2b3dc82ce
masum184e <masum184e@gmail.com>
2023-12-30 14:32:24 +0600

    min max added

---
+++ Vector/find.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> sized_vector(10);
+
+    // FIND MAXIMUM ELEMENT
+    cout<<"Max Element = "<<*max_element(a.begin(),a.end())<<endl;
+
+    // FIND MINIMUM ELEMENT
+    cout<<"Min Element = "<<*min_element(a.begin(),a.end())<<endl;
+ 
+    return 0;
+}
