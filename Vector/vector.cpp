9c685ec66472e840df9f55531ab8c8a5e51ac5f1
masum184e <masum184e@gmail.com>
2023-12-30 12:04:39 +0600

    empty vector declaration added

---
+++ Vector/vector.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  // EMPTY VECTOR
+  vector<int> empty_vector;
+  // You can only perform push operation, it doesn't accept indexing
+
+  return 0;
+}
