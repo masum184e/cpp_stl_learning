b9cd49174152b7434b84aad1decc59f9f1d83ed5
masum184e <masum184e@gmail.com>
2023-12-31 15:49:20 +0600

    binary search added

--- Searching/searching.cpp
+++ Searching/searching.cpp
@@ -7,7 +7,13 @@ int main(){
     // FIND SPECIFIC ELEMENT
     auto iterator=find(initial_value_vector.begin(), initial_value_vector.end(), 6);
     if(iterator!=initial_value_vector.end())cout<<"Element found at index "<<iterator-initial_value_vector.begin()<<endl;
-    else cout<<"Element not found in the vector."<<endl;
+    else cout<<"Element not found in the vector."<<endl;    
+
+    // BINARY SEARCH
+    bool status=binary_search(initial_value_vector.begin(), initial_value_vector.end(),6);
+    (status)?cout<<"Found"<<endl:cout<<"Not Found"<<endl;
+    // set of data must be sorted
+    // binary_search return boolean value
 
     return 0;
 }
