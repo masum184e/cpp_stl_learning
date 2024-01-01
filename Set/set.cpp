5bce53aaddd8022549fe08338fa1c2d89d398377
masum184e <masum184e@gmail.com>
2024-01-01 20:41:03 +0600

    set added

---
+++ Set/set.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+    // DECLARATION
+    set<int> decSet;
+
+    // INITIALIZATION
+    set<int> initSet={3,1,4,1,5,9,2,6,5};
+    set<int> constructorSet({3,1,4,1,5,9,2,6,5});
+
+    // ADD ELEMENT
+    decSet.insert(1);
+
+    // ACCESSING
+    for(auto i:initSet)cout<<i<<" ";
+
+    // SEARCHING
+    (initSet.find(5)!=initSet.end())?cout<<"Found"<<endl:cout<<"Not Found"<<endl;
+
+    // RANDOM FUNCTIONS
+    initSet.size();
+
+    return 0;
+}
