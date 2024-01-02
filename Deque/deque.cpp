d6c9ce75cec0b56512b966263c8985d5b33225bb
masum184e <masum184e@gmail.com>
2024-01-02 23:56:44 +0600

    insertion added

--- Deque/deque.cpp
+++ Deque/deque.cpp
@@ -8,5 +8,9 @@ int main(){
     // INITIALIZATION
     deque<int> initDeq={ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
+    // ADD ELEMENT
+    initDeq.push_back(11); // add element at the end
+    initDeq.push_front(-1); // add element at the front
+
     return 0;
 }
